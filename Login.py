8f3d72826662d58abbc01d2dd0d560345db41820
JCal25 <126228112+JCal25@users.noreply.github.com>
2023-05-19 17:17:26 +0800

    Update Login.py

--- Login.py
+++ Login.py
@@ -1,8 +1,21 @@
 from tkinter import *
 from tkinter import messagebox
 from PIL import Image, ImageTk
-import pymysql
 import os
+import pyrebase
+
+config = {
+  "apiKey": "AIzaSyAqlITRDZ3gaw5rHhy9hUCwN4xAUDT-svc",
+  "authDomain": "epbip-17adb.firebaseapp.com",
+  "databaseURL": "https://epbip-17adb-default-rtdb.asia-southeast1.firebasedatabase.app",
+  "projectId": "epbip-17adb",
+  "storageBucket": "epbip-17adb.appspot.com",
+  "messagingSenderId": "612338602406",
+  "appId": "1:612338602406:web:dd0e8e6d1f905f5d60ff67",
+  "measurementId": "G-J1896JVRT9"
+}
+firebase = pyrebase.initialize_app(config)
+database = firebase.database()
 
 # window
 login = Tk()
@@ -11,38 +24,53 @@ login.geometry("400x420")
 
 
 def on_entry(e):
-    user.delete(0, 'end')
+    usernm.configure(show="")
+    if usernm.get() == "Email":
+        usernm.configure(foreground="black")
+        usernm.delete(0, 'end')
 
 
 def on_password(e):
-    name = user.get()
-    if name == '':
-        user.insert(0, 'Email')
-
+    if usernm.get() == "":
+        usernm.configure(show="", foreground="grey")
+        usernm.delete(0, "end")
+        usernm.insert(0, "Email")
+    else:
+        usernm.configure(show="", foreground="black")
 
 def on_enter(e):
-    passw.delete(0, 'end')
+    passw.configure(show="*")
+    if passw.get() == "Password":
+        passw.configure(foreground="black")
+        passw.delete(0, "end")
 
 
 def on_leave(e):
-    name = passw.get()
-    if name == '':
-        passw.insert(0, 'Password')
+    if passw.get() == "":
+        passw.configure(show="", foreground="grey")
+        passw.delete(0, "end")
+        passw.insert(0, "Password")
+    else:
+        passw.configure(show="*", foreground="black")
+        
 
 
 def invalid():
-        try:
-            con=pymysql.connect(host='localhost', user='root', password='Pangthesis2023@', database='EPBIP')
-            cur=con.cursor()
-            cur.execute('select * from register where emailid=%s and password=%s',(user.get(), passw.get()))
-            row=cur.fetchone()
-            if row == None:
-                messagebox.showerror("Error", "Invalid Email or Password!")
+    if usernm.get() == 'Email' and passw.get() == 'Password':
+        messagebox.showerror("Error", "No input in the field")
+    elif usernm.get() == 'Email' or passw.get() == 'Password':
+        messagebox.showerror("Error", "No input in other field")
+    else:
+        users=database.child("Users").get()
+        for user in users.each():
+            if  usernm.get() != user.val() or passw.get() != user.val():
+                messagebox.showerror("Error", "Invalid Email or Password")
             else:
+                messagebox.showinfo("Success", "Login successful!")
                 login.destroy()
                 os.system("Dashboard.py")
-        except Exception as es:
-            messagebox.showerror("Error",f'Error Due to : {str(es)}')
+             
+           
 
 def toreg():
     login.destroy()
@@ -59,18 +87,12 @@ bg_0 = Image.open("img\\bg.jpg")
 bck_pk = ImageTk.PhotoImage(bg_0.resize((400, 420)))
 
 lbl = Label(login, image=bck_pk, border=0)
-lbl.place(x=1, y=1)
-
-
-# Header
-box_1 = Frame(login, width=400, height=55, bg='#010F57')
-box_1.place(x=3, y=5)
-heading = Label(login, text='SIGN IN', fg='white', bg='#010F57', font=('Arial', 30, 'bold'))
-heading.place(x=130, y=5)
+lbl.place(x=0, y=0, relwidth=1, relheight=1)
+lbl.pack()
 
 # logo
 box_2 = Frame(login, width=300, height=320, bg='#010F57')
-box_2.place(x=50, y=80)
+box_2.place(x=50, y=50)
 
 logo = Image.open("img\\logoo.png")
 log_pk = ImageTk.PhotoImage(logo.resize((105, 100)))
@@ -82,14 +104,14 @@ log_name = Label(box_2, text='E.P.B.I.P', fg='white', bg='#010F57', font=('Coppe
 log_name.place(x=100, y=100)
 
 # user
-user = Entry(box_2, width=25, fg='grey', border=1, bg='white', font=('Arial', 11, 'bold'))
-user.place(x=50, y=140)
-user.insert(0, ' Email')
-user.bind('<FocusIn>', on_entry)
-user.bind('<FocusOut>', on_password)
+usernm = Entry(box_2, width=25, fg='grey', border=1, bg='white', font=('Arial', 11, 'bold'), show="")
+usernm.place(x=50, y=140)
+usernm.insert(0, 'Email')
+usernm.bind('<FocusIn>', on_entry)
+usernm.bind('<FocusOut>', on_password)
 
 # password
-passw = Entry(box_2, width=25, fg='black', border=1, bg='white', font=('Arial', 11, 'bold'), show='*')
+passw = Entry(box_2, width=25, fg='grey', border=1, bg='white', font=('Arial', 11, 'bold'),show="")
 passw.place(x=50, y=175)
 passw.insert(0, 'Password')
 passw.bind('<FocusIn>', on_enter)
